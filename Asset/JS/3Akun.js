351610cebce2f75c79dd527e97b73d929928a705
= <=>
2025-05-24 12:50:35 +0700

    upload pelanggan file

--- Asset/JS/3Akun.js
+++ Asset/JS/3Akun.js
@@ -3,3 +3,11 @@ function togglePassword() {
   const pw = document.getElementById("password");
   pw.type = pw.type === "password" ? "text" : "password";
 }
+
+function ketikaClick() {
+  document.querySelector(".popup3").style.display = "block";
+}
+
+function closed() {
+  document.querySelector(".popup3").style.display = "none";
+}
